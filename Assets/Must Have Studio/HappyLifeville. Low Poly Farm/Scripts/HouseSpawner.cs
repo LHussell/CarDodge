3af379d04e618cc8b65dd76479b287f69c266a93
Lloyd Hussell <lloydhussell@gmail.com>
2021-08-22 16:10:03 +0200

    added people assets. Fixed houses not being destroyed.

--- Assets/Must Have Studio/HappyLifeville. Low Poly Farm/Scripts/HouseSpawner.cs
+++ Assets/Must Have Studio/HappyLifeville. Low Poly Farm/Scripts/HouseSpawner.cs
@@ -3,11 +3,15 @@ using UnityEngine;
 
 public class HouseSpawner : MonoBehaviour
 {
+
+    GameObject house;
+
     public void buildHouses(GameObject gameObject)
     {
+        Destroy(house);
         System.Random random = new System.Random();
         GameObject houseLocation = gameObject.transform.Find("House_Location" + random.Next(1, 5)).gameObject;
-        var house = Instantiate(Resources.Load("House_" + random.Next(1, 5)) as GameObject, houseLocation.transform.position, transform.rotation);
+        house = Instantiate(Resources.Load("House_" + random.Next(1, 5)) as GameObject, houseLocation.transform.position, transform.rotation);
         house.transform.parent = houseLocation.transform;
     }
 }
