722e2907f979996553695fa2cd8922b59a75ab0e
Lloyd Hussell <lloydhussell@gmail.com>
2021-09-03 17:33:43 +0200

    Introduced better roads

--- Assets/Scripts/HouseSpawner.cs
+++ Assets/Scripts/HouseSpawner.cs
@@ -10,7 +10,7 @@ public class HouseSpawner : MonoBehaviour
     {
         Destroy(house);
         System.Random random = new System.Random();
-        GameObject houseLocation = gameObject.transform.Find("House_Location" + random.Next(1, 5)).gameObject;
+        GameObject houseLocation = gameObject.transform.Find("House_Location" + random.Next(1, 3)).gameObject;
         house = Instantiate(Resources.Load("House_" + random.Next(1, 5)) as GameObject, houseLocation.transform.position, transform.rotation);
         house.transform.parent = houseLocation.transform;
         EnemySpawner enemySpawner = gameObject.GetComponent<EnemySpawner>();
