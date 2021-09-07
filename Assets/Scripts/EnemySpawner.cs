7db8bc957e25107d4b9cab2f42d2f456d1f2bbae
Lloyd Hussell <lloydhussell@gmail.com>
2021-09-07 20:00:22 +0200

    Added new vehicles and animations for shooting weapons.

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour
 {
 
-    System.Random random;
+    public GameObject enemy;
 
-    private void Awake()
-    {
-        random = new System.Random();
-    }
     public void SpawnEnemyAt(GameObject gameObject)
     {
         GameObject enemySpawnLocation = gameObject.transform.Find("Lane_Location" + RandomNumGen.instance.GetRandomNumber(1, 4)).gameObject;
-        Instantiate(Resources.Load("Cars/Car_" + RandomNumGen.instance.GetRandomNumber(1, 4)) as GameObject, enemySpawnLocation.transform.position, transform.rotation * Quaternion.Euler(0f, 180f, 0f));
+        GameObject enemyCar = Instantiate(Resources.Load("Cars/Car_1") as GameObject, enemySpawnLocation.transform.position, transform.rotation * Quaternion.Euler(0f, 180f, 0f));
+        if (RandomNumGen.instance.GetRandomNumber(1,4) == 1 && enemy != null)
+        {
+            GameObject enemySpawned = Instantiate(enemy, enemyCar.transform.Find("EnemySpawnPoint").transform.position + new Vector3(0, -0.5f, 0), transform.rotation * Quaternion.Euler(0f, 180f, 0f));
+            enemySpawned.transform.parent = enemyCar.transform.Find("EnemySpawnPoint").transform;
+        }
     }
 }
